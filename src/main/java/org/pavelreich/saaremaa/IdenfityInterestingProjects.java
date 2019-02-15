0e171bf427eb833daa18da680a60352a0e8a78c8
preich <preich@192.168.1.101>
2019-02-15 01:23:30 +0000

    projects with jacoco in pom.xml are more suitable

--- src/main/java/org/pavelreich/saaremaa/IdenfityInterestingProjects.java
+++ src/main/java/org/pavelreich/saaremaa/IdenfityInterestingProjects.java
@@ -1,7 +1,10 @@
 package org.pavelreich.saaremaa;
 
+import java.io.BufferedReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.stream.Collectors;
 import java.util.zip.ZipFile;
 
@@ -12,29 +15,42 @@ import java.util.zip.ZipFile;
  */
 public class IdenfityInterestingProjects {
 
+	
 	static boolean goodEnough(String f) {
 		try {
-			var files = new ZipFile(f).stream().collect(Collectors.toList());
+			ZipFile zf = new ZipFile(f);
+			var files = zf.stream().collect(Collectors.toList());
 			boolean pom = false;
 			boolean tests = false;
 			for (var x : files) {
 				if (x.getName().contains("pom.xml")) {
-					pom = true;
+					InputStream is = zf.getInputStream(x);
+					pom = isJacocoInside(is);
 				}
 				if (x.getName().contains("Test.java")) {
 					tests = true;
 				}
 			}
+			zf.close();
 			return pom && tests;
 		} catch (Exception e) {
+			e.printStackTrace();
 			return false;
 		}
+	}
+
+	private static boolean isJacocoInside(InputStream is) throws IOException {
+		String pluginName = "jacoco-maven-plugin";
+		//final String pluginName = "maven-shade-plugin";
+		long count = new BufferedReader(new InputStreamReader(is)).lines().filter(s->s.contains(pluginName)).count();
+		is.close();
+		return count > 0;
 	}
 
 	public static void main(String[] args) throws IOException {
 		var zipFiles = java.nio.file.Files.walk(java.nio.file.Paths.get(".")).filter(p -> p.toFile().toString().endsWith(".zip")).collect(Collectors.toList());
 
-		FileWriter fw = new FileWriter("suitable-projects.txt");
+		FileWriter fw = new FileWriter("suitable-projects.txt1");
 		var goodFiles = zipFiles.parallelStream().filter(p -> goodEnough(p.toString())).map(x -> {
 			try {
 				fw.write(x+"\n");
