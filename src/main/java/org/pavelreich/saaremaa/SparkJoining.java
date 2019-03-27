a64c0ae5c8f675a11a400fc1d55f67d171abb59d
preich <preich@192.168.1.101>
2019-03-27 00:52:21 +0000

    spark join

---
+++ src/main/java/org/pavelreich/saaremaa/SparkJoining.java
@@ -0,0 +1,43 @@
+package org.pavelreich.saaremaa;
+
+import static org.apache.spark.sql.functions.callUDF;
+
+import java.util.Arrays;
+
+import org.apache.spark.sql.Dataset;
+import org.apache.spark.sql.Row;
+import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.api.java.UDF1;
+import org.apache.spark.sql.types.DataTypes;
+
+public class SparkJoining {
+	private static UDF1 extractProject = new UDF1<String, String>() {
+		public String call(final String fname) throws Exception {
+			return fname.replaceAll(".*?\\./([^/]+/[^/]+).*", "$1");
+		}
+	};
+
+	public static void main(String[] args) {
+
+		SparkSession spark = SparkSession.builder().appName("CSV to Dataset").master("local[*]").getOrCreate();
+		// Reads a CSV file with header, called books.csv, stores it in a dataframe
+		String dir = args[0];//"/Users/preich/Documents/git/phdp/";
+		Dataset<Row> mocks = spark.read().format("csv").option("delimiter", ";").option("header", "true")
+				.load(dir + "results.csv").drop("LOC");
+
+		Dataset<Row> metrics = spark.read().format("csv").option("delimiter", ";").option("header", "true")
+				.load(dir + "class-metrics.csv").drop("loc");
+
+		spark.udf().register("extractProject", extractProject, DataTypes.StringType);
+		metrics = metrics.withColumn("project", callUDF("extractProject", metrics.col("file")));
+		mocks = mocks.withColumn("project", callUDF("extractProject", mocks.col("fileName")));
+		mocks = mocks.withColumn("class", mocks.col("mockClass"));
+		Dataset<Row> ret = mocks.join(metrics, 		scala.collection.JavaConverters.asScalaIteratorConverter(
+		        Arrays.asList("project", "class").iterator()).asScala().toSeq(),"outer");
+		ret.show();
+		    
+		System.out.println("count: " + ret.count());
+		ret.toJavaRDD().saveAsTextFile(dir+"merged.csv");
+
+	}
+}
